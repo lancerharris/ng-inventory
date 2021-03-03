9a8ed1aa96966231f1e4f13c148f83a258d18b28
Lance Harris <lancerprogram@gmail.com>
2021-03-03 09:12:19 -0500

    Remove sidenav autofocus
    to remove the templates button highlighting

--- src/app/Item-Management/add-item/add-item.component.ts
+++ src/app/Item-Management/add-item/add-item.component.ts
@@ -1,29 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { MatSidenav } from '@angular/material/sidenav';
 
 import { ItemManagementService } from '../item-management.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-add-item',
   templateUrl: './add-item.component.html',
-  styleUrls: ['./add-item.component.css']
+  styleUrls: ['./add-item.component.css'],
 })
 export class AddItemComponent implements OnInit {
-  
-  templates: string[] = ["Tops", "Dresses", "Bottoms"]
-  addingTemplate: boolean;
-  value: string = '';
+  public templates: string[] = ['Tops', 'Dresses', 'Bottoms'];
+  public addingTemplate: boolean;
+  public value: string = '';
+  public reason: string = '';
 
-  constructor(private itemManager: ItemManagementService, private router: Router) { }
+  constructor(
+    private itemManager: ItemManagementService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.addingTemplate = this.router.url === '/items/add-template';
   }
 
   onAddInput() {
-    const currentLength = this.itemManager.totalInputs.length
+    const currentLength = this.itemManager.totalInputs.length;
     if (currentLength <= 19) {
       // to keep track of the indices of the input elements
       this.itemManager.totalInputs.push(currentLength);
@@ -35,14 +39,18 @@ export class AddItemComponent implements OnInit {
     const fields: string[] = [];
     const values: string[] = [];
 
-    this.itemManager.totalInputs.forEach(index => {
-      const fieldInput =  document.getElementById('field_' + index) as HTMLInputElement;
-      const valueInput = document.getElementById('value_' + index) as HTMLInputElement
+    this.itemManager.totalInputs.forEach((index) => {
+      const fieldInput = document.getElementById(
+        'field_' + index
+      ) as HTMLInputElement;
+      const valueInput = document.getElementById(
+        'value_' + index
+      ) as HTMLInputElement;
       fields[index] = fieldInput.value;
       values[index] = valueInput.value;
     });
-    
-    const gemInput = {fields: fields, values: values };
+
+    const gemInput = { fields: fields, values: values };
     const longField = form.value.longField ? form.value.longField : null;
 
     console.log(gemInput);
@@ -50,9 +58,8 @@ export class AddItemComponent implements OnInit {
     let addItemObs: Observable<{
       data: { createGem: { fields: string[]; values: string[] } };
     }> = this.itemManager.addItem(gemInput, longField);
-    
-    addItemObs.subscribe((resData) => {
-    });
+
+    addItemObs.subscribe((resData) => {});
 
     // form.reset();
   }
