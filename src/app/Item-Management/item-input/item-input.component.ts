b9db15ca8faf231645cb074ff7443b4ab804fc09
Lance Harris <lancerprogram@gmail.com>
2021-03-09 10:13:32 -0500

    Refactor long click to get rid of afterViewChecked

--- src/app/Item-Management/item-input/item-input.component.ts
+++ src/app/Item-Management/item-input/item-input.component.ts
@@ -15,7 +15,7 @@ import { ItemInputService } from '../item-input.service';
   templateUrl: './item-input.component.html',
   styleUrls: ['./item-input.component.css'],
 })
-export class ItemInputComponent implements OnInit, OnDestroy, AfterViewChecked {
+export class ItemInputComponent implements OnInit, OnDestroy {
   @Input() editMode: boolean = false;
 
   public itemFields: string[];
@@ -24,8 +24,7 @@ export class ItemInputComponent implements OnInit, OnDestroy, AfterViewChecked {
   public totalInputs: number[];
   public longField: boolean = false;
   public priorLongFieldIndex: number;
-  public longFieldIndex: number;
-  public longFieldValue: string;
+  public longFieldIndex: number = -1;
   private templateSelectSub: Subscription;
   private longFieldSub: Subscription;
   private inputAddedSub: Subscription;
@@ -39,7 +38,6 @@ export class ItemInputComponent implements OnInit, OnDestroy, AfterViewChecked {
     this.itemFields = this.itemInputService.itemFields;
     this.itemValues = this.itemInputService.itemValues;
     this.totalInputs = this.itemInputService.totalInputs;
-    this.longFieldIndex = this.itemInputService.getLongFieldIndex();
     this.longFieldSub = this.itemInputService.longFieldSubject.subscribe(
       (index) => {
         this.longFieldIndex = index;
@@ -62,19 +60,22 @@ export class ItemInputComponent implements OnInit, OnDestroy, AfterViewChecked {
 
   onClearItem(index) {
     if (index === this.longFieldIndex) {
-      this.onLongClick(index);
+      this.longFieldIndex = -1;
     }
     this.itemInputService.removeInputs(index, 1);
   }
 
   onLongClick(index) {
-    this.longFieldValue = this.itemInputService.itemValues[index];
-    this.longField = !this.longField;
-    if (this.longField) {
-      this.itemInputService.setLongFieldIndex(index);
+    const value = this.itemInputService.itemValues[index] || '';
+    if (this.longFieldIndex > -1) {
+      this.itemInputService.itemValues[index] = value.substring(
+        0,
+        this.itemInputService.MAX_VALUE_LENGTH
+      );
+      this.longFieldIndex = -1;
     } else {
-      this.itemInputService.setLongFieldIndex(-1);
-      this.priorLongFieldIndex = index;
+      this.itemInputService.itemValues[index] = value;
+      this.longFieldIndex = index;
     }
   }
 
@@ -166,18 +167,6 @@ export class ItemInputComponent implements OnInit, OnDestroy, AfterViewChecked {
       (event.key === 'ArrowLeft' || event.key === 'ArrowRight')
     ) {
       this.onInputLeftRight(event.key, startIndex, inputStatus);
-    }
-  }
-
-  ngAfterViewChecked(): void {
-    if (this.priorLongFieldIndex > -1) {
-      this.itemInputService.itemValues[
-        this.priorLongFieldIndex
-      ] = this.longFieldValue.substring(
-        0,
-        this.itemInputService.MAX_VALUE_LENGTH
-      );
-      this.priorLongFieldIndex = -1;
     }
   }
 
