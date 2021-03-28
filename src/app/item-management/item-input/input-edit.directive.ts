511c2d3f8530bd160b0e678c7e357aebb3fa1368
Lance Harris <lancerprogram@gmail.com>
2021-03-28 15:02:55 -0400

    Fix review item editing functionality.

--- src/app/item-management/item-input/input-edit.directive.ts
+++ src/app/item-management/item-input/input-edit.directive.ts
@@ -1,9 +1,7 @@
 import { Directive, ElementRef, HostListener, Input } from '@angular/core';
-import { fromEvent, Observable } from 'rxjs';
-import { debounceTime, distinctUntilChanged, map, tap } from 'rxjs/operators';
-import { ItemCrudService } from '../item-crud.service';
+import { BehaviorSubject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { ReviewItemsService } from '../review-items.service';
-import { TableManagementService } from '../table-management.service';
 
 @Directive({
   selector: '[appInputEdit]',
@@ -11,55 +9,48 @@ import { TableManagementService } from '../table-management.service';
 export class InputEditDirective {
   @Input() reviewMode: boolean;
 
-  private typeahead: Observable<{
+  private typeaheadSubject: BehaviorSubject<{
     text: string;
     fieldOrValue: string;
     rowIndex: string;
   }>;
-  private temp: string;
+
+  private listnerInitialized: boolean = false;
+
   constructor(
     private el: ElementRef,
     private reviewItemsService: ReviewItemsService
   ) {}
 
   @HostListener('input') onInput() {
-    this.setUpListener();
-  }
+    const idSplit = (this.el.nativeElement as HTMLInputElement).id.split('_');
+    const eventObject = {
+      text: (this.el.nativeElement as HTMLInputElement).value,
+      fieldOrValue: idSplit[0],
+      rowIndex: idSplit[1],
+    };
 
-  setUpListener() {
-    console.log('hello der');
-    console.log(this.reviewMode);
     if (this.reviewMode) {
-      this.temp = 'hello';
-      this.typeahead = fromEvent(this.el.nativeElement, 'input').pipe(
-        tap((e) => console.log(e)),
-        map((e: InputEvent) => {
-          const idSplit = (e.target as HTMLInputElement).id.split('_');
-          return {
-            text: (e.target as HTMLInputElement).value,
-            fieldOrValue: idSplit[0],
-            rowIndex: idSplit[1],
-          };
-        }),
-        debounceTime(50),
-        distinctUntilChanged((curr, prev) => curr.text === prev.text)
-      );
-
-      this.typeahead.subscribe((ev) => {
-        console.log('ev in sub');
-        console.log(ev);
-        const currItem = this.reviewItemsService.currItem;
-        const currItemKey = ev.fieldOrValue === 'field' ? 'fields' : 'values';
-
-        if (ev.text !== currItem[currItemKey][ev.rowIndex]) {
-          this.el.nativeElement.classList.add('cell__edited');
-        }
-      });
+      if (!this.listnerInitialized) {
+        this.typeaheadSubject = new BehaviorSubject(eventObject);
+        this.typeaheadSubject.pipe(
+          debounceTime(50),
+          distinctUntilChanged((curr, prev) => curr.text === prev.text)
+        );
+      }
+      this.typeaheadSubject.next(eventObject);
+      this.listnerInitialized = true;
     }
-  }
 
-  @HostListener('mouseenter') onMouseEnter() {
-    console.log(this.reviewMode);
-    console.log(this.temp);
+    this.typeaheadSubject.subscribe((ev) => {
+      const currItem = this.reviewItemsService.currItem;
+      const currItemKey =
+        eventObject.fieldOrValue === 'field' ? 'fields' : 'values';
+      if (eventObject.text !== currItem[currItemKey][eventObject.rowIndex]) {
+        this.el.nativeElement.classList.add('cell__edited');
+      } else {
+        this.el.nativeElement.classList.remove('cell__edited');
+      }
+    });
   }
 }
